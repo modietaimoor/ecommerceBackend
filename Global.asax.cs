d6c1f5412799c706df4e338fb3c0920530e682db
modietaimoor <123105495+modietaimoor@users.noreply.github.com>
2024-01-24 05:29:39 +0200

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace eCommerceApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_BeginRequest()
+        {
+            if (Request.HttpMethod == "OPTIONS")
+            {
+                Response.StatusCode = (int)HttpStatusCode.OK;
+                Response.AppendHeader("Access-Control-Allow-Origin", "*");
+                Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept,authorizationheader");
+                Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                Response.AppendHeader("Access-Control-Allow-Credentials", "true");
+                Response.AppendHeader("Access-Control-Max-Age", "1728000");
+
+                Response.Flush();
+
+                HttpContext.Current.ApplicationInstance.CompleteRequest();
+
+                //Response.End();
+            }
+        }
+    }
+}
